458d76dcb1c65fcddd67f648bdf25659ae1badc9
Bram Vanbilsen <bramvbilsen@gmail.com>
2019-04-29 17:03:43 +0200

    Dart 2.0 compatible

--- lib/geolocation.dart
+++ lib/geolocation.dart
@@ -3,57 +3,57 @@ import 'dart:collection';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:geohash/geohash.dart';
 
-
 import './geoquery.dart';
 
 class FirebaseGeoLocation {
+  DatabaseReference _dbRef;
+  String _path;
+  GeoQuery query;
+  HashSet<GeoQuery> queries = new HashSet();
 
-    DatabaseReference _dbRef;
-    String _path;
-    GeoQuery query;
-    HashSet<GeoQuery> queries = new HashSet();
+  FirebaseGeoLocation(this._path) {
+    _dbRef = FirebaseDatabase.instance.reference().child(path);
+  }
 
-    FirebaseGeoLocation(this._path) {
-        _dbRef = FirebaseDatabase.instance.reference().child(path);
+  String get path => this._path;
+
+  Future<void> setLocation(String key, List<double> position) {
+    if (position[0] > 90 || position[0] < -90) {
+      throw new ArgumentError.value(position[0], "latitude",
+          "The latitude argument has to be a double between -90 and 90 (inclusive)");
     }
-
-    String get path => this._path;
-
-    Future<Null> setLocation(String key, List<double> position) {
-        if (position[0] > 90 || position[0] < -90) {
-            throw new ArgumentError.value(position[0], "latitude", "The latitude argument has to be a double between -90 and 90 (inclusive)");
-        }
-        if (position[1] > 180 || position[1] < -180) {
-            throw new ArgumentError.value(position[1], "longitude", "The longitude argument has to be a double between -180 and 180 (inclusive)");
-        }
-        return _dbRef.child(key).set(<String, dynamic>{
-            "hash": Geohash.encode(position[0], position[1]),
-            "loc": position
-        });
+    if (position[1] > 180 || position[1] < -180) {
+      throw new ArgumentError.value(position[1], "longitude",
+          "The longitude argument has to be a double between -180 and 180 (inclusive)");
     }
+    return _dbRef.child(key).set(<String, dynamic>{
+      "hash": Geohash.encode(position[0], position[1]),
+      "loc": position
+    });
+  }
 
-    Future<List<double>> getLocation(String key) {
-        return _dbRef.child(key).once().then((DataSnapshot snap) {
-            List<double> loc = snap.value["loc"];
-            return loc;
-        });
-    }
+  Future<List<double>> getLocation(String key) {
+    return _dbRef.child(key).once().then((DataSnapshot snap) {
+      List<double> loc = snap.value["loc"];
+      return loc;
+    });
+  }
 
-    Future<Null> removeLocation(String key) {
-        return _dbRef.child(key).remove();
-    }
+  Future<void> removeLocation(String key) {
+    return _dbRef.child(key).remove();
+  }
 
-    GeoQuery addGeoQuery(List<double> center, double radius) {
-        GeoQuery query = new GeoQuery(_path, center, radius);
-        queries.add(query);
-        return query;
-    }
+  GeoQuery addGeoQuery(List<double> center, double radius) {
+    GeoQuery query = new GeoQuery(_path, center, radius);
+    queries.add(query);
+    return query;
+  }
 
-    void removeGeoQuery(GeoQuery query) {
-        if (!queries.contains(query))
-            throw new ArgumentError("The query you are trying to delete is not attached to this object!");
-        query.removeGeoQueryEventListener();
-        queries.remove(query);
-    }
-    
+  void removeGeoQuery(GeoQuery query) {
+    if (!queries.contains(query))
+      throw new ArgumentError(
+          "The query you are trying to delete is not attached to this object!");
+    query.removeGeoQueryEventListener();
+    queries.remove(query);
+  }
 }
--- lib/geoquery.dart
+++ lib/geoquery.dart
@@ -9,20 +9,19 @@ import 'package:geohash/geohash.dart';
 part './geohashquery.dart';
 
 class GeoQuery {
-
   DatabaseReference _dbRef;
   List<double> _center;
   double _radius;
   String _path;
-	String _geoHash;
+  String _geoHash;
 
-	HashSet<StreamSubscription> subscriptions = new HashSet();
-  
+  HashSet<StreamSubscription> subscriptions = new HashSet();
+
   GeoQuery(String path, List<double> center, double radius) {
-		this._center = center;
-		this._radius = radius;
-		this._geoHash = Geohash.encode(center[0], center[1]);
-		this._path = path;
+    this._center = center;
+    this._radius = radius;
+    this._geoHash = Geohash.encode(center[0], center[1]);
+    this._path = path;
     _dbRef = FirebaseDatabase.instance.reference().child(path);
   }
 
@@ -31,92 +30,98 @@ class GeoQuery {
   String get path => _path;
 
   void addGeoQueryEventListener(GeoQueryEventListener listener) {
+    String newPrecisionHash;
+    List neighbors;
 
-		String newPrecisionHash;
-		List neighbors;
+    if (radius <= 0.6) {
+      // Geohash precision ~6
+      newPrecisionHash = _geoHash.substring(0, 5);
+    } else if (radius <= 2.5) {
+      // Geohash precision ~5
+      newPrecisionHash = _geoHash.substring(0, 4);
+    } else if (radius <= 20) {
+      // Geohash precision ~4
+      newPrecisionHash = _geoHash.substring(0, 3);
+    } else if (radius <= 78) {
+      // Geohash precision ~3
+      newPrecisionHash = _geoHash.substring(0, 2);
+    }
 
-		if (radius <= 0.6) { // Geohash precision ~6
-			newPrecisionHash = _geoHash.substring(0, 5);
-		} else if (radius <= 2.5) { // Geohash precision ~5
-			newPrecisionHash = _geoHash.substring(0, 4);
-		} else if (radius <= 20) { // Geohash precision ~4
-			newPrecisionHash = _geoHash.substring(0, 3);
-		} else if (radius <= 78) { // Geohash precision ~3
-			newPrecisionHash = _geoHash.substring(0, 2);
-		}
+    neighbors = [
+      _GeoHashQuery.adj(newPrecisionHash, "n"),
+      _GeoHashQuery.adj(newPrecisionHash, "e"),
+      _GeoHashQuery.adj(newPrecisionHash, "s"),
+      _GeoHashQuery.adj(newPrecisionHash, "w"),
+      _GeoHashQuery.adj(_GeoHashQuery.adj(newPrecisionHash, 'n'), 'e'),
+      _GeoHashQuery.adj(_GeoHashQuery.adj(newPrecisionHash, 's'), 'e'),
+      _GeoHashQuery.adj(_GeoHashQuery.adj(newPrecisionHash, 's'), 'w'),
+      _GeoHashQuery.adj(_GeoHashQuery.adj(newPrecisionHash, 'n'), 'w'),
+    ];
 
-		neighbors = [
-			_GeoHashQuery.adj(newPrecisionHash, "n"),
-			_GeoHashQuery.adj(newPrecisionHash, "e"),
-			_GeoHashQuery.adj(newPrecisionHash, "s"),
-			_GeoHashQuery.adj(newPrecisionHash, "w"),
-			_GeoHashQuery.adj(_GeoHashQuery.adj(newPrecisionHash, 'n'), 'e'),
-			_GeoHashQuery.adj(_GeoHashQuery.adj(newPrecisionHash, 's'), 'e'),
-			_GeoHashQuery.adj(_GeoHashQuery.adj(newPrecisionHash, 's'), 'w'),
-			_GeoHashQuery.adj(_GeoHashQuery.adj(newPrecisionHash, 'n'), 'w'),
-		];
-
-		for (int i = 0; i < 9; i++) {
-			String hashToUse = i == 0 ? newPrecisionHash : neighbors[i-1]; // Use hash for appropriate location
-			Query query = _dbRef.orderByChild("hash")
-						.startAt(hashToUse)
-						.endAt(hashToUse + "\uf8ff");
-			subscriptions.add(query.onChildAdded.listen((Event e) {
-				List<double> loc = e.snapshot.value["loc"];
-				if (_distance(center[0], center[1], loc[0], loc[1], "K") <= radius) {
-					listener.onKeyEntered(e.snapshot.key, loc);
-				}
-			}));
-			subscriptions.add(query.onChildRemoved.listen((Event e) {
-				List<double> loc = e.snapshot.value["loc"];
-				print(loc);
-				if (_distance(center[0], center[1], loc[0], loc[1], "K") <= radius) {
-					listener.onKeyExited(e.snapshot.key);
-				}
-			}));
-			subscriptions.add(query.onChildChanged.listen((Event e) {
-				List<double> loc = e.snapshot.value["loc"];
-				print(loc);
-				if (e.snapshot.value["hash"].toString().startsWith(hashToUse)) {
-					if (_distance(center[0], center[1], loc[0], loc[1], "K") <= radius) {
-						listener.onKeyMoved(e.snapshot.key, loc);
-					}
-				} else {
-					listener.onKeyExited(e.snapshot.key);
-				}
-			}));
-		}
-
+    for (int i = 0; i < 9; i++) {
+      String hashToUse = i == 0
+          ? newPrecisionHash
+          : neighbors[i - 1]; // Use hash for appropriate location
+      Query query = _dbRef
+          .orderByChild("hash")
+          .startAt(hashToUse)
+          .endAt(hashToUse + "\uf8ff");
+      subscriptions.add(query.onChildAdded.listen((Event e) {
+        List<double> loc = e.snapshot.value["loc"];
+        if (_distance(center[0], center[1], loc[0], loc[1], "K") <= radius) {
+          listener.onKeyEntered(e.snapshot.key, loc);
+        }
+      }));
+      subscriptions.add(query.onChildRemoved.listen((Event e) {
+        List<double> loc = e.snapshot.value["loc"];
+        print(loc);
+        if (_distance(center[0], center[1], loc[0], loc[1], "K") <= radius) {
+          listener.onKeyExited(e.snapshot.key);
+        }
+      }));
+      subscriptions.add(query.onChildChanged.listen((Event e) {
+        List<double> loc = e.snapshot.value["loc"];
+        print(loc);
+        if (e.snapshot.value["hash"].toString().startsWith(hashToUse)) {
+          if (_distance(center[0], center[1], loc[0], loc[1], "K") <= radius) {
+            listener.onKeyMoved(e.snapshot.key, loc);
+          }
+        } else {
+          listener.onKeyExited(e.snapshot.key);
+        }
+      }));
+    }
   }
 
   void removeGeoQueryEventListener() {
-  	subscriptions.forEach((StreamSubscription sub) {
-  		sub.cancel();
-		});
-  	subscriptions.clear();
-	}
+    subscriptions.forEach((StreamSubscription sub) {
+      sub.cancel();
+    });
+    subscriptions.clear();
+  }
 
-	double _distance(double lat1, double lon1, double lat2, double lon2, String unit) {
-		double theta = lon1 - lon2;
-		double dist = sin(_deg2rad(lat1)) * sin(_deg2rad(lat2)) + cos(_deg2rad(lat1)) * cos(_deg2rad(lat2)) * cos(_deg2rad(theta));
-		dist = acos(dist);
-		dist = _rad2deg(dist);
-		dist = dist * 60 * 1.1515;
-		if (unit == "K") {
-			dist = dist * 1.609344;
-		} else if (unit == "N") {
-			dist = dist * 0.8684;
-		}
+  double _distance(
+      double lat1, double lon1, double lat2, double lon2, String unit) {
+    double theta = lon1 - lon2;
+    double dist = sin(_deg2rad(lat1)) * sin(_deg2rad(lat2)) +
+        cos(_deg2rad(lat1)) * cos(_deg2rad(lat2)) * cos(_deg2rad(theta));
+    dist = acos(dist);
+    dist = _rad2deg(dist);
+    dist = dist * 60 * 1.1515;
+    if (unit == "K") {
+      dist = dist * 1.609344;
+    } else if (unit == "N") {
+      dist = dist * 0.8684;
+    }
 
-		return (dist);
-	}
+    return (dist);
+  }
 
-	double _deg2rad(double deg) {
-		return (deg * PI / 180.0);
-	}
+  double _deg2rad(double deg) {
+    return (deg * pi / 180.0);
+  }
 
-	double _rad2deg(double rad) {
-		return (rad * 180 / PI);
-	}
-
+  double _rad2deg(double rad) {
+    return (rad * 180 / pi);
+  }
 }
